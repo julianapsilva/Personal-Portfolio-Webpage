4f1170134054f89b8b415e850670c45b9ed9ad80
julianapsilva <julianadepaula001@gmail.com>
2021-05-08 14:39:23 -0300

    adicionando responsividade

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const t = document.getElementById("text-welcome")
-const text = document.getElementById("text-welcome").textContent
+const t = document.getElementById("text-welcome");
+const text = document.getElementById("text-welcome").textContent;
 
 let index = 0;
-/*
-t.addEventListener(onclick, writeText)
-function writeText() {
-     document.body.innerText = text.slice(0, index);
-    index++;
 
-    if (index > text.length) {
-        index = 0;
-    }
-   
+function writeText() {
+  t.innerText = text.slice(0, index);
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
 }
 
-setInterval(writeText, 100);
-*/
+setInterval(writeText, 160);
