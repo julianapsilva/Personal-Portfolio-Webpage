a8184654903913bf6b84fe654b9166af45daa924
julianapsilva <julianadepaula001@gmail.com>
2021-05-07 21:58:48 -0300

    novo layout

---
+++ app.js
@@ -0,0 +1,18 @@
+const t = document.getElementById("text-welcome")
+const text = document.getElementById("text-welcome").textContent
+
+let index = 0;
+/*
+t.addEventListener(onclick, writeText)
+function writeText() {
+     document.body.innerText = text.slice(0, index);
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+   
+}
+
+setInterval(writeText, 100);
+*/
